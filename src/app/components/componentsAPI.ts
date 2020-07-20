77831f29c80e2e6aa1b6be9082752ba437df1a15
Adepitan Moshood <mkoabiola95@gmail.com>
2020-07-20 15:16:04 +0100

    apis

---
+++ src/app/components/componentsAPI.ts
@@ -0,0 +1,6 @@
+import { supportRequestsAPI } from './supportRequests';
+import { usersAPI } from './users';
+
+export { authenticationAPI } from './authentication';
+
+export const componentsAPI = [usersAPI, supportRequestsAPI];
