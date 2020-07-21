7960af69093c9bb661a51278d4dd21fbe4c321b8
Adepitan Moshood <mkoabiola95@gmail.com>
2020-07-21 18:07:44 +0100

    fixes on exporttocsv

--- src/app/helpers/exportToCSV.ts
+++ src/app/helpers/exportToCSV.ts
@@ -3,7 +3,7 @@ import { saveFile } from './saveFile';
 
 interface ExportToCSVDTO {
   data: any;
-  fields: string[];
+  fields: any[];
   name?: string;
 }
 export async function exportToCSV({
@@ -12,7 +12,7 @@ export async function exportToCSV({
   name,
 }: ExportToCSVDTO): Promise<{ fileName: string }> {
   const csvData = await parseAsync(data, { fields });
-  let fileName = name ? name : 'tams_downloads';
+  let fileName = name ? name : 'fliqpay_csts_downloads';
 
   if (!fileName.split('.').includes('csv')) {
     fileName = `${fileName}.csv`;
